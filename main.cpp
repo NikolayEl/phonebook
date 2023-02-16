e9ad09718e99854e94a3a46df1f71b56f643c7e9
Starinin Andrey <starinin-andrey@ya.ru>
2023-02-16 15:53:39 +0300

    testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <string>
+
+#include "Person.h"
+#include "PhoneBook.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Person person1;
+    person1.last_name = "S";
+    person1.first_name = "A";
+    person1.patronymic = "N";
+    person1.sex = 'M';
+    person1.date_of_birth.year = 1;
+    person1.date_of_birth.month = 1;
+    person1.date_of_birth.day = 1;
+    person1.tel = "1234567890";
+
+    Person person2;
+    person2.last_name = "S";
+    person2.first_name = "A";
+    person2.patronymic = "N";
+    person2.sex = 'M';
+    person2.date_of_birth.year = 1;
+    person2.date_of_birth.month = 1;
+    person2.date_of_birth.day = 1;
+    person2.tel = "1234567890";
+
+    //cout << person1.ToString();
+    //cout << person2.ToString();
+
+    PhoneBook phoneBook;
+    phoneBook.Add(person1);
+    phoneBook.Add(person2);
+
+    try {
+        auto result = phoneBook.FindByLastName("S");
+        for (auto person : result) {
+            cout << person.ToString();
+        }
+    } catch (...) {
+        cerr << "Person not found" << endl;
+    }
+
+
     return 0;
 }
